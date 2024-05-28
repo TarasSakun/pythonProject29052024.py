c2a14bff0861ab6c6aa397890c1161a09edb2064
TarasSakun <ndt2007@ukr.net>
2024-05-28 22:46:03 +0300

    Add files via upload

--- Python Console.py
+++ Python Console.py
@@ -1,6 +1,16 @@
-user_input = input("Введите 4х значное число: ")
-number = int(user_input)
-print(number // 1000)
-print((number // 100) % 10)
-print((number // 10) % 10)
-print(number % 10)
+def reverse_number(number):
+    digit_1 = number // 10000
+    digit_2 = (number // 1000) % 10
+    digit_3 = (number // 100) % 10
+    digit_4 = (number // 10) % 10
+    digit_5 = number % 10
+
+    reversed_number = digit_5 * 10000 + digit_4 * 1000 + digit_3 * 100 + digit_2 * 10 + digit_1
+
+    return reversed_number
+
+user_input = int(input("Введите 5-ти значное число: "))
+
+reversed_result = reverse_number(user_input)
+
+print(f"Результат: {reversed_result}")
